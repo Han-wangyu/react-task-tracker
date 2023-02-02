1a73631b1151cf67dad95dd4e66398da91502e98
hanwangyu <hanwangyu@foxmail.com>
2023-02-02 22:06:41 +0800

    Page Frame Done

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import React from "react";
+import Button from "./Button";
+
+const Header = (props) => {
+    const onClick = () => {
+        console.log('Click');
+    }
+
+    return (
+        <header className='header'>
+            <h1>{props.title}</h1>
+            <Button color='green' text='Add' onClick={onClick}></Button>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// };
+
+export default Header
