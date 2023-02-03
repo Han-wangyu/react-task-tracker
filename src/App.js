06799b20f134c1205d886c15849c4ae701137182
hanwangyu <hanwangyu@foxmail.com>
2023-02-03 19:30:52 +0800

    Delete Task

--- src/App.js
+++ src/App.js
@@ -23,10 +23,16 @@ function App() {
             reminder: false
         },
     ]);
+
+    // Delete Task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    }
+
   return (
     <div className="container">
       <Header></Header>
-        <Tasks tasks={tasks}></Tasks>
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}></Tasks> : 'No Tasks'}
     </div>
   );
 }
